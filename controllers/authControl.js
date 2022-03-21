59c0ec888cddaf8d16fc2580d1ac404a18f7ad3b
Javdad <javokhirbekparpiev@gmail.com>
2022-03-21 05:13:33 +0500

    HW-4 add test. Change some code

--- controllers/authControl.js
+++ controllers/authControl.js
@@ -1,8 +1,10 @@
 const bcrypt = require("bcrypt");
 const comparePassword = require("./handlers/comparePassword");
 const generateToken = require("./handlers/generateToken");
+const errorHandler = require("../utils/errorHandler");
 
 const User = require("../models/user");
+const loginValidator = require("../validators/authValidator");
 
 class AuthController {
 	registerUsers = async (req, res) => {
@@ -30,19 +32,23 @@ class AuthController {
 
 	loginUser = async (req, res) => {
 		try {
+			const validate = loginValidator(req.body);
+			if (validate.error) return res.status(400).send(validate.error.message);
+
 			const { email, password } = req.body;
+
 			const candidate = await User.findOne({ email });
 			if (!candidate)
-				res.status(404).json({
-					massage: `Account with this emal: ${email} doesn't exist!`,
+				return res.status(404).json({
+					massage: `Account with this email: ${email} doesn't exist!`,
 				});
 			if (!comparePassword(password, candidate))
-				res.status(400).json({ massage: "Password is wrong!" });
+				return res.status(400).json({ massage: "Password is wrong!" });
 
 			const token = generateToken(candidate);
-			return res.json({ token: `Bearer ${token}` });
+			return res.status(200).json({ token: `Bearer ${token}` });
 		} catch (err) {
-			res.json({ massage: "Please, enter the password!" });
+			errorHandler(res, err);
 		}
 	};
 
